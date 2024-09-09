e2120cb10fc8e2ef6bcd3da0fb6654f1022797d0
octavioricci <octavioricci@gmail.com>
2024-09-08 23:22:32 -0400

    Corrijo_script

--- test/main.py
+++ test/main.py
@@ -1,8 +1,11 @@
 import os,sys
 
 def calc(num1:int, num2:int):
-    return num1/num2
+    try:
+        print(num1/num2)
+    except ZeroDivisionError:
+        print("No se puede dividir un nro por 0")
 
 if __name__ == '__main__':
-    result = calc(int(sys.argv[1]),int(sys.argv[2]))
-    print(result)
+    calc(int(sys.argv[1]),int(sys.argv[2]))
+    
