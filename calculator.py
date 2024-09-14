03540620b45344d2187dc2c8eee4181953346392
bluecatsecurity <bluecatsecurity@yandex.com>
2024-09-14 01:35:25 -0300

    correciones_segun_flake8

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
 import sys
 
-def sumar(num1:int,num2:int):
+
+def sumar(num1: int,num2: int):
     return num1+num2
 
-def restar(num1:int,num2:int):
+def restar(num1: int,num2: int):
     return num1-num2
 
 if __name__ == '__main__':
